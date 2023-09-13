ba9e15f809bee3a3943e4cd47f83902c6e750046
AhsenGvrcn <99078465@mydavinci.nl>
2023-09-13 12:43:46 +0200

    uitwerkingen les 12

---
+++ Deel_2/Deel_2/Les_12/uitwerkingen/les_12_opdr_2.py
@@ -0,0 +1,9 @@
+getal_1_str = input("Wat is de waarde van getal 1?")
+getal_2_str = input("Wat is de waarde van getal 2?")
+
+getal_1 = float(getal_1_str)
+getal_2 = float(getal_2_str)
+
+gemiddelde = (getal_1 + getal_2) / 2
+
+print(f'HEt gemmiddelde van {getal_1} en {getal_2} is {gemiddelde}')
